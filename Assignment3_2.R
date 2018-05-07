05b096f52d98994ad272d8d2dfc8f2ab3a6c1f4d
DineshKumar <dkdinesh22@gmail.com>
2018-05-07 23:54:57 +0530

    Add files via upload

---
+++ Assignment3_2.R
@@ -0,0 +1,12 @@
+
+#3.2
+
+#1 Question
+
+mymat<-matrix(rep(c(1:5),4),nrow = 4,ncol = 5) #defineing matrix with replicating values 1 to 5 four time
+mymat;  
+
+
+#O/P
+rowSums(mymat)  # sum of rows 
+colSums(mymat)  # sum of column
